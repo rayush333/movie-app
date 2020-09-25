a3b2e559fd63470b753714d17a006fc1c2aae2c1
Santhosh Kurian <a-8068@LE1421.local>
2020-09-25 11:59:41 +0530

    Auth logout Implementation

--- movie_app_react/src/components/Navbars/AdminNavbarLinks.jsx
+++ movie_app_react/src/components/Navbars/AdminNavbarLinks.jsx
@@ -1,24 +1,21 @@
-/*!
 
-=========================================================
-* Light Bootstrap Dashboard React - v1.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React, { Component } from "react";
 import { NavItem, Nav, NavDropdown, MenuItem } from "react-bootstrap";
 
 class AdminNavbarLinks extends Component {
+  constructor(props) {
+    super("123",props);
+    console.log(props)
+    this.logout = this.logout.bind(this)
+    this.state = {name:localStorage.getItem('name')}
+  }
+
+  logout(){
+console.log("logout");
+this.props.history.push('/login');
+localStorage.clear();
+
+  }
   render() {
     const notification = (
       <div>
@@ -53,23 +50,11 @@ class AdminNavbarLinks extends Component {
           </NavItem>
         </Nav>
         <Nav pullRight>
-          <NavItem eventKey={1} href="#">
-            Account
+          <NavItem eventKey={1}>
+            Welcome {this.state.name} 
           </NavItem>
-          <NavDropdown
-            eventKey={2}
-            title="Dropdown"
-            id="basic-nav-dropdown-right"
-          >
-            <MenuItem eventKey={2.1}>Action</MenuItem>
-            <MenuItem eventKey={2.2}>Another action</MenuItem>
-            <MenuItem eventKey={2.3}>Something</MenuItem>
-            <MenuItem eventKey={2.4}>Another action</MenuItem>
-            <MenuItem eventKey={2.5}>Something</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey={2.5}>Separated link</MenuItem>
-          </NavDropdown>
-          <NavItem eventKey={3} href="#">
+
+          <NavItem eventKey={2} onClick={this.logout}>
             Log out
           </NavItem>
         </Nav>
